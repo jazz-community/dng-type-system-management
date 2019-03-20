477530373d6a1447e72d74fb3e6b19cd7a09e6b6
Ralph Schoon <rsjazz777@gmail.com>
2019-03-20 09:25:56 +0100

    Version Update, different packaging to reduce initial load time.

--- com.ibm.rm.typemanagement/scripts/tsm.bat
+++ com.ibm.rm.typemanagement/scripts/tsm.bat
@@ -1,3 +1,5 @@
+@echo off
 rem set JAVA_HOME=C:\PROGRA~1\Java\jre1.8.0_181
+rem set JAVA_HOME=C:\Program Files (x86)\Java\jre1.8.0_191
 
-%JAVA_HOME%\bin\java -jar tsm.jar %*
+"%JAVA_HOME%\bin\java" -cp ./tsm_lib -jar tsm.jar %*
--- com.ibm.rm.typemanagement/scripts/tsm.sh
+++ com.ibm.rm.typemanagement/scripts/tsm.sh
@@ -2,4 +2,4 @@
 #JAVA_HOME=/usr/java/jre1.8.0_181
 #export JAVA_HOME
 
-$JAVA_HOME/bin/java -cp ./lib -jar tsm.jar "$@"
+$JAVA_HOME/bin/java -cp ./tsm_lib -jar tsm.jar "$@"
