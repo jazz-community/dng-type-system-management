b3cdf80354225c1cd8bb7d7e4a15ef7cc35435c5
Ralph Schoon <ralph.schoon@de.ibm.com>
2021-07-28 08:49:28 +0200

    Exception handling

--- com.ibm.rm.typemanagement/src/main/java/com/ibm/requirement/typemanagement/oslc/client/dngcm/InternalConfigurationArchiveApi.java
+++ com.ibm.rm.typemanagement/src/main/java/com/ibm/requirement/typemanagement/oslc/client/dngcm/InternalConfigurationArchiveApi.java
@@ -5,12 +5,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 
+import org.apache.http.client.HttpClient;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.wink.client.ClientResponse;
 import org.eclipse.lyo.client.oslc.jazz.JazzFormAuthClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +16,15 @@ import org.slf4j.LoggerFactory;
 import com.ibm.requirement.typemanagement.oslc.client.resources.Configuration;
 
 import net.oauth.OAuthException;
-import net.oauth.http.HttpClient;
 
+/**
+ * Internal API implementation to archive configurations 
+ *
+ */
 public class InternalConfigurationArchiveApi {
 	
 	public static final Logger logger = LoggerFactory.getLogger(InternalConfigurationArchiveApi.class);
+	public static final String localVersioning = "/localVersioning/configurations/archiveWithDescendants";
 
 	public static boolean archiveWithDescendants(final JazzFormAuthClient client, final Configuration configuration)
 			throws IOException, OAuthException, URISyntaxException {
@@ -39,24 +41,23 @@ public class InternalConfigurationArchiveApi {
 		boolean result = false;
 		if (configuration == null) {
 			logger.info("Change set must not be null");
-			return false;
+			return result;
 		}
-		String localVersioning = "/localVersioning/configurations/archiveWithDescendants";
 		//POST https://jazz.ibm.com:9443/rm/localVersioning/configurations/archiveWithDescendants?configurationUri=https%3A%2F%2Fjazz.ibm.com%3A9443%2Frm%2Fcm%2Fbaseline%2F_UdEZMN_9EeuEmPSHUJVGfg HTTP/1.1
 		String url = client.getUrl();
 		String param = null;
 		try {
 			 param = URLEncoder.encode(configuration, "UTF-8");
-		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			logger.error("Exception unsupported encoding: " + e.getMessage());
+			return result;
 		}
-		String finalUrl = url.concat(localVersioning).concat("?").concat("configurationUri=").concat(param);
+		final String finalUrl = url.concat(localVersioning).concat("?").concat("configurationUri=").concat(param);
 		
 		logger.debug("archive with descendants");
 		HttpResponse response = null;
 		try {
-			org.apache.http.client.HttpClient rawClient = client.getHttpClient();
+			HttpClient rawClient = client.getHttpClient();
 			HttpUriRequest archive = new HttpPost(finalUrl);
 			archive.addHeader("Accept", "*/*");
 			response = rawClient.execute(archive);
@@ -67,7 +68,7 @@ public class InternalConfigurationArchiveApi {
 			 * 
 			 * Behavior of POST
 			 * 
-			 * 200 discarded.
+			 * 200 archived.
 			 *
 			 * 
 			 */
